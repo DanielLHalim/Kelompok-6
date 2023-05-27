a7e757c774ee121d28f735a8f3d8fa85c1d95972
JeffersonSutanto23 <jefferson.liu888@gmail.com>
2023-05-27 15:18:47 +0700

    tugas
    tugas

---
+++ Kelompok 6/src/HISTORYTRANSFER.java
@@ -0,0 +1,8 @@
+public class HISTORYTRANSFER {
+        /* history transfer merupakan suatu fitur yang menunjukkan beberapa jejak-jejak dari beberapa jenis operasi
+           yang dilakukan oleh pengguna , contohnya seperti : withdraw , transfer , deposit.Dan dari history transfer,
+           kita dapat melihat data-data berupa jumlah uang yang ditransaksi , nomor telepon tujuan transaksi , dan
+           tanggal dan waktu kita melakukan operasi itu.
+          */
+   } 
+
