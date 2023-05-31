f86d7e33ecf72e48966176b2d706576ea8849e39
kerstynlaigusten <kerstynlaigusten@gmail.com>
2023-05-31 20:11:12 +0700

    Transaction
    mendeskripsikan transaksi

---
+++ Kelompok 6/src/Transaction.java
@@ -0,0 +1,7 @@
+public class Transaction {
+    /*
+    - didalam transaksi dimulai dengan memasukan dan mengidentifikasikan data berupa jenis transaksi yang dilakukan.
+    - apabila sudah menentukan jenis transaksi yang mau dibayar maka kita akan memproses pembayaran
+    - setelah kita memilih transaksi yang akan dilakukan maka sistem akan menentukan jumlah saldo akun dan melakukan proses pembayaran dan memotong saldo akun*/
+    }
+}
