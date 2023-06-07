e5ba2e86bb1dee148e58bedc6b1e40223ace9a88
DanielLHalim <daniel06juli@gmail.com>
2023-06-07 18:05:50 +0700

    Product.java
    Product

--- Kelompok 6/src/Product.java
+++ Kelompok 6/src/Product.java
@@ -1,8 +1,29 @@
 public class Product {
-    private String namaProduct
-    private String totalBayar
-    private String saldo
-    
-    
-    
+    private String namaProduct;
+    private String totalBayar;
+    private String saldo;
+
+    public String getNamaProduct() {
+        return this.namaProduct;
+    }
+
+    public void setNamaProduct(String namaProduct) {
+        this.namaProduct = namaProduct;
+    }
+
+    public String getTotalBayar() {
+        return this.totalBayar;
+    }
+
+    public void setTotalBayar(String totalBayar) {
+        this.totalBayar = totalBayar;
+    }
+
+    public String getSaldo() {
+        return this.saldo;
+    }
+
+    public void setSaldo(String saldo) {
+        this.saldo = saldo;
+    }
 }
