cc58d0b68302006a48c54ff515be62a024a9703f
Davidsen C <69659578+Zk04@users.noreply.github.com>
2023-06-14 00:30:28 +0700

    Setoran

--- Kelompok 6/src/Setoran.java
+++ Kelompok 6/src/Setoran.java
@@ -1,24 +1,15 @@
 public class Setoran{
-    public String JenisSetoran;
     public String JumlahSetoran;
     public String Saldo;
 
     public Setoran() {
     }
 
-    public Setoran(String JenisSetoran, String JumlahSetoran, String Saldo) {
-        this.JenisSetoran = JenisSetoran;
+    public Setoran(String JumlahSetoran, String Saldo) {
         this.JumlahSetoran = JumlahSetoran;
         this.Saldo = Saldo;
     }
 
-    public String getJenisSetoran() {
-        return this.JenisSetoran;
-    }
-
-    public void setJenisSetoran(String JenisSetoran) {
-        this.JenisSetoran = JenisSetoran;
-    }
 
     public String getJumlahSetoran() {
         return this.JumlahSetoran;
@@ -39,7 +30,6 @@ public class Setoran{
     @Override
     public String toString() {
         return "{" +
-            " JenisSetoran='" + getJenisSetoran() + "'" +
             ", JumlahSetoran='" + getJumlahSetoran() + "'" +
             ", Saldo='" + getSaldo() + "'" +
             "}";
